55bcbdbb20dc69fa7b0f8cbbe35e056ef0356537
michael.kysa <michael.kysa@gmail.com>
2023-04-11 04:09:30 -0400

    added banckhets

---
+++ src/constants/sections.ts
@@ -0,0 +1,8 @@
+export const sectionBankets = {
+  sectionId: "banchete",
+  sectionName: "Banchete",
+  img: '/banchete.png',
+  descriptionOne: 'Vă invităm să vă sărbătoriți evenimentul la PIZZA Ferma cu Origini - un loc unic unde puteți petrece un timp de neuitat alături de familia și prietenii tăi.',
+  descriptionTwo: 'Unitatea noastră oferă o mare varietate de feluri de mâncare, servicii excelente și o atmosferă confortabilă pentru a vă face să vă simțiți ca acasă.',
+  descriptionThree: 'Nu aștepta prea mult, rezervă azi și organizează-ți următorul banchet cu noi!'
+};
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,64 +1,114 @@
-import Menu from '@/components/menu';
-import ProductCard from '@/components/ProductCard';
-import SectionDividerBox from '@/components/SectionDividerBox';
-import Supplements from '@/components/supplements';
-import { MenuSection, Product, products, translatedProducts } from '@/constants/products';
-import { Box } from '@mui/material';
-import ButtonScrollUp from '@/components/buttonScrollUp';
+import Menu from "@/components/menu";
+import ProductCard from "@/components/ProductCard";
+import SectionDividerBox from "@/components/SectionDividerBox";
+import Supplements from "@/components/supplements";
+import {
+  MenuSection,
+  Product,
+  products,
+  translatedProducts,
+} from "@/constants/products";
+import { Box, Typography } from "@mui/material";
+import ButtonScrollUp from "@/components/buttonScrollUp";
+import { sectionBankets } from "@/constants/sections";
+import Image from "next/image";
 
 const MainView = () => {
   return (
-    <Box pt={'55px'}>
+    <Box pt={"55px"}>
       <Menu />
-      <Box display={"flex"} flexDirection={"column"} gap={10} mt={4} >
-        {translatedProducts.map((section: MenuSection, sectionIndex: number) => (
-          <SectionDividerBox
-            sectionName={section.sectionName}
-            key={section.sectionName}
-            id={section.id}
-          >
-            <Box
-              display={'flex'}
-              flexWrap={'wrap'}
-              alignItems={'stretch'}
-              gap={{
-                xs: '20px',
-                sm: '44px',
-                md: '80px',
-                lg: '44px'
-              }}>
-              {section.products?.map((product: Product, index) => {
-                return (
+      <Box display={"flex"} flexDirection={"column"} gap={10} mt={4}>
+        {translatedProducts.map(
+          (section: MenuSection, sectionIndex: number) => (
+            <SectionDividerBox
+              sectionName={section.sectionName}
+              key={section.sectionName}
+              id={section.id}
+            >
+              <Box
+                display={"flex"}
+                flexWrap={"wrap"}
+                alignItems={"stretch"}
+                gap={{
+                  xs: "20px",
+                  sm: "44px",
+                  md: "80px",
+                  lg: "44px",
+                }}
+              >
+                {section.products?.map((product: Product, index) => {
+                  return (
+                    <Box
+                      key={product.name + index}
+                      width={{
+                        xs: "calc((100% - 20px) / 2)",
+                        sm: "calc((100% - 44px) / 2)",
+                        md: "calc((100% - 160px) / 3)",
+                        lg: "calc((100% - 132px) / 4)",
+                      }}
+                    >
+                      <ProductCard item={product} />
+                    </Box>
+                  );
+                })}
+                {products.length - 1 === sectionIndex && (
                   <Box
-                    key={product.name + index}
                     width={{
-                      xs: 'calc((100% - 20px) / 2)',
-                      sm: 'calc((100% - 44px) / 2)',
-                      md: 'calc((100% - 160px) / 3)',
-                      lg: 'calc((100% - 132px) / 4)'
-                    }} >
-                    <ProductCard item={product} />
+                      xs: "100%",
+                      sm: "100%",
+                      md: "calc(((100% - 80px) / 3) * 2)",
+                      lg: "calc(((100% - 88px) / 4) * 2)",
+                    }}
+                  >
+                    <Supplements />
                   </Box>
-                );
-              })}
-              {
-                (products.length - 1 === sectionIndex) &&
-                <Box
-                  width={{
-                    xs: '100%',
-                    sm: '100%',
-                    md: 'calc(((100% - 80px) / 3) * 2)',
-                    lg: 'calc(((100% - 88px) / 4) * 2)'
-                  }}
-                >
-                  <Supplements />
-                </Box>
-              }
+                )}
+              </Box>
+            </SectionDividerBox>
+          )
+        )}
+
+        <SectionDividerBox
+          sectionName={sectionBankets.sectionName}
+          key={sectionBankets.sectionName}
+          id={sectionBankets.sectionId}
+        >
+          <Box display={"flex"} flexDirection={"column"}>
+            <Box
+              display={"flex"}
+              gap={"45px"}
+              flexDirection={{ xs: "column", md: "row" }}
+            >
+              <Box
+                width={1}
+                display={"flex"}
+                flexDirection={"column"}
+                gap={"30px"}
+              >
+                <Typography component={"p"} variant="h3">
+                  {sectionBankets.descriptionOne}
+                </Typography>
+                <Typography component={"p"} variant="h3">
+                  {sectionBankets.descriptionTwo}
+                </Typography>
+                <Typography component={"p"} variant="h3">
+                  {sectionBankets.descriptionThree}
+                </Typography>
+              </Box>
+              <Box width={1}>
+                <Image
+                  layout="responsive"
+                  width={520}
+                  height={288}
+                  src={sectionBankets.img}
+                  alt={sectionBankets.sectionName}
+                />
+              </Box>
             </Box>
-          </SectionDividerBox>
-        ))}
+          </Box>
+        </SectionDividerBox>
       </Box>
-      <ButtonScrollUp/>
+      <ButtonScrollUp />
     </Box>
   );
 };
