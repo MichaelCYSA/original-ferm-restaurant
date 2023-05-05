1d0c221b9891779f47b9262cf3cd7267eafec272
michael.kysa <wertygo1997@gmail.com>
2023-05-05 03:25:47 -0400

    fix

--- src/layout/AdminLayout/AdminLayout.tsx
+++ src/layout/AdminLayout/AdminLayout.tsx
@@ -22,25 +22,6 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import LayuotContainer from "@/components/Containers/LayuotContainer";
 import { useRouter } from "next/router";
 
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
@@ -77,7 +58,7 @@ const AdminLayout = ({ children }: { children: JSX.Element }) => {
             //boxSizing: "border-box",
           },
         }}
-        variant="persistent"
+      //  variant="persistent"
         anchor="left"
         open={open}
       >
