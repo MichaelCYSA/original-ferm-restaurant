a9ce10b5a627883ab23c2d9ff88decf5cf1d25b1
michael.kysa <wertygo1997@gmail.com>
2023-06-08 07:00:43 -0400

    fixed congih

---
+++ config/development.ts
@@ -0,0 +1,7 @@
+const config = {
+  BASE_URL:
+    process.env.BASE_URL || "https://original-ferm-backend.vercel.app/api",
+  PRODUCT_IMG_HOST: process.env.PRODUCT_IMG_HOST || "/product-photos",
+};
+
+export default config;
