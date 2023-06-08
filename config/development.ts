f0130a85ade85ece0436723c249ee03a2b49c09e
michael.kysa <wertygo1997@gmail.com>
2023-06-08 07:17:27 -0400

    fixed

--- config/development.ts
+++ config/development.ts
@@ -1,7 +1,11 @@
+// const config = {
+//   BASE_URL:
+//     process.env.BASE_URL || "https://original-ferm-backend.vercel.app/api",
+//   PRODUCT_IMG_HOST: process.env.PRODUCT_IMG_HOST || "/product-photos",
+// };
 const config = {
-  BASE_URL:
-    process.env.BASE_URL || "https://original-ferm-backend.vercel.app/api",
-  PRODUCT_IMG_HOST: process.env.PRODUCT_IMG_HOST || "/product-photos",
+  BASE_URL: "http://178.17.170.142:9000/api",
+  PRODUCT_IMG_HOST: "http://178.17.170.142/product-photos",
 };
 
 export default config;
