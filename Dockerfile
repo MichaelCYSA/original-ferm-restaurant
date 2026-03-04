35742b864cdec9771d0700215ec3779967bfa4a6
michael.kysa <michael.kysa@gmail.com>
2026-03-04 08:07:25 -0500

    f

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM node:20-alpine AS builder
+RUN apk add --no-cache libc6-compat
+WORKDIR /app
+
+COPY package.json package-lock.json* ./
+RUN npm install
+
+COPY . .
+RUN npm run build
+
+FROM node:20-alpine AS runner
+RUN apk add --no-cache libc6-compat
+
+WORKDIR /app
+ENV NODE_ENV=production
+
+COPY --from=builder /app/.next/standalone   ./
+COPY --from=builder /app/.next/static       ./.next/static
+COPY --from=builder /app/public             ./public        
+
+RUN addgroup --system --gid 1001 nodejs && \
+    adduser --system --uid 1001 nextjs && \
+    chown -R nextjs:nodejs /app && \
+    mkdir -p .next/cache/images && \
+    chown -R nextjs:nodejs .next
+
+USER nextjs
+
+ENV PORT=3000
+ENV HOSTNAME="0.0.0.0"
+
+EXPOSE 3000
+CMD ["node", "server.js"]
---
+++ bin/start.sh
@@ -0,0 +1,59 @@
+set -euo pipefail
+
+IMAGE_NAME="original-ferm-front"
+
+CONTAINER_NAME="original-ferm-front-3000"
+
+PORT_HOST=3000
+PORT_CONTAINER=3000
+
+ENV_VAR="NEXT_PUBLIC_API_ENDPOINT=https://fermacuorigini.pizza/api"
+
+DOCKERFILE_PATH="../Dockerfile"
+
+if [[ ! -f "${DOCKERFILE_PATH}" ]]; then
+    echo "Not found: Dockerfile path ${DOCKERFILE_PATH}"
+    exit 1
+fi
+
+echo "Creating ${IMAGE_NAME} ..."
+echo ""
+
+docker build \
+    -f "${DOCKERFILE_PATH}" \
+    -t "${IMAGE_NAME}" \
+    "$(dirname "${DOCKERFILE_PATH}")" || {
+        echo ""
+        echo "Image stopped with error ↑"
+        echo "Fix problems"
+        exit 1
+    }
+
+echo ""
+echo "Iamge ${IMAGE_NAME} was created / updated"
+echo ""
+
+
+docker stop  "${CONTAINER_NAME}" >/dev/null 2>&1 || true
+docker rm    "${CONTAINER_NAME}" >/dev/null 2>&1 || true
+
+echo ""
+echo "Start container..."
+echo "Container name:${CONTAINER_NAME}"
+echo "Base api url:${ENV_VAR}"
+echo ""
+
+docker run -d \
+    --name "${CONTAINER_NAME}" \
+    -p "${PORT_HOST}:${PORT_CONTAINER}" \
+    --env-file ../.env \
+    --restart unless-stopped \
+    "${IMAGE_NAME}"
+
+echo ""
+echo "═══════════════════════════════════════════════════════════════"
+echo "                          READY"
+echo "═══════════════════════════════════════════════════════════════"
+echo ""
+echo "Server:"
+echo "http://localhost:${PORT_HOST}"
